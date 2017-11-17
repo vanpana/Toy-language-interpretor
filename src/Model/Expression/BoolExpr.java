41e657c871c56fa2eb48064b687f2da55f045827
vanpana <van.panaite@gmail.com>
2017-11-17 13:32:02 +0200

    Created BoolExpr
    exp1 < exp2
    exp1<=exp2
    exp1==exp2
    exp1!=exp2
    exp1> exp2
    exp1>=exp2

---
+++ src/Model/Expression/BoolExpr.java
@@ -0,0 +1,50 @@
+package Model.Expression;
+
+import Model.ADT.Interfaces.MyIDictionary;
+import Model.ADT.Interfaces.MyIHeap;
+import Model.Exceptions.ToyException;
+
+public class BoolExpr extends Expr{
+    private Expr expr1, expr2;
+    private String operator;
+
+    public BoolExpr(Expr expr1, Expr expr2, String operator) {
+        this.expr1 = expr1;
+        this.expr2 = expr2;
+        this.operator = operator;
+    }
+
+    @Override
+    public int eval(MyIDictionary<String, Integer> symTable, MyIHeap<Integer> heap) throws ToyException {
+        int eval1 = expr1.eval(symTable, heap), eval2 = expr2.eval(symTable, heap), result = -1;
+        switch (operator)
+        {
+            case "<":
+                result = eval1 < eval2 ? 1 : 0;
+                break;
+            case "<=":
+                result = eval1 <= eval2 ? 1 : 0;
+                break;
+            case "==":
+                result = eval1 == eval2 ? 1 : 0;
+                break;
+            case "!=":
+                result = eval1 != eval2 ? 1 : 0;
+                break;
+            case ">":
+                result = eval1 > eval2 ? 1 : 0;
+                break;
+            case ">=":
+                result = eval1 >= eval2 ? 1 : 0;
+                break;
+        }
+
+        if (result == -1) throw new ToyException("Can't evaluate: " + this.toString());
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return expr1 + " " + operator + " " + expr2;
+    }
+}
